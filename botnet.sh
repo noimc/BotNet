f4e6707c580c93b31752cae18e6facca7a86b06f
Noi MC <yutynetwork2011@gmail.com>
2025-05-17 21:33:03 +0700

    Update botnet.sh

--- botnet.sh
+++ botnet.sh
@@ -22,4 +22,20 @@ echo ""
 PACKAGE="gói cần tải"
 echo "Đang cài đặt $PACKAGE..."
 apt -y update -o=Dpkg::Progress-Fancy=1 -qq && apt -y install wget -o=Dpkg::Progress-Fancy=1 -qq && apt -y install -o=Dpkg::Progress-Fancy=1 -qq && apt -y install openjdk-21-jdk -o=Dpkg::Progress-Fancy=1 -qq
-wget --progress=bar:force -q 
+wget --progress=bar:force -q https://raw.githubusercontent.com/noimc/BotNet/refs/heads/main/index.js
+wget --progress=bar:force -q https://github.com/noimc/BotNet/raw/refs/heads/main/Botnet.jar
+wget --progress=bar:force -q https://raw.githubusercontent.com/noimc/BotNet/refs/heads/main/proxies.txt
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+\. "$HOME/.nvm/nvm.sh"
+nvm install 22
+node -v
+nvm current
+
+clear
+NO_FORMAT="\033[0m"
+F_BOLD="\033[1m"
+C_DODGERBLUE1="\033[48;5;33m"
+echo -e "${F_BOLD}${C_DODGERBLUE1}Ｂｏｔｎｅｔ － Ｎｏｉ ＭＣ${NO_FORMAT}"
+echo ""
+echo "Hãy đăng nhập vào link http://localhost:3000"
+node index.js
